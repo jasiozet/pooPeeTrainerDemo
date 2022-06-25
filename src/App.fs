344a5e16f53994452123ab31bd950123d065fb69
Jan Zalewski <18668118+jasiozet@users.noreply.github.com>
2022-06-25 10:15:30 +0200

    using commands

--- src/App.fs
+++ src/App.fs
@@ -1,13 +1,20 @@
 module App
+open Elmish
 open Feliz
 open Feliz.DaisyUI
+open Logic
+open System
 
-type State =
-    { Count: int }
+type State = {
+    Count: int
+    Pup : Pup
+    Events : Event list
+}
 
 type Msg =
     | Increment
     | Decrement
+    | SaveState
 
 let defaultValue count =
     if count = null then
@@ -15,21 +22,49 @@ let defaultValue count =
     else
         count |> int
 
-let init() =
-    { Count = Browser.WebStorage.localStorage.getItem("count") |> defaultValue }
+let storeStateInLocalStorage state =
+    Browser.WebStorage.localStorage.setItem("count", string(state.Count))
 
-let update (msg: Msg) (state: State): State =
+let init() =
+    {
+      Count = Browser.WebStorage.localStorage.getItem("count") |> defaultValue;
+      Pup = {Name = "Lusia"; Birthday=DateOnly(04,05,2022); Gender=Gender.Female};
+      Events = []
+    },
+    Cmd.none
+
+let update (msg: Msg) (state: State): State * Cmd<Msg> =
     match msg with
     | Increment ->
-        Browser.WebStorage.localStorage.setItem("count", string(state.Count + 1))
-        { state with Count = state.Count + 1 }
-
+        { state with Count = state.Count + 1 }, Cmd.ofMsg SaveState
     | Decrement ->
-        Browser.WebStorage.localStorage.setItem("count", string(state.Count - 1))
-        { state with Count = state.Count - 1 }
+        { state with Count = state.Count - 1 }, Cmd.ofMsg SaveState
+    | SaveState ->
+        storeStateInLocalStorage state
+        state, Cmd.none
 
 let render (state: State) (dispatch: Msg -> unit) =
   Html.div [
+    Html.div [
+      prop.classes [ "w-96" ]
+      prop.children [
+        Daisy.card [
+          card.bordered
+          card.full
+          prop.children [
+            Html.figure [
+              Html.text "🐕"
+            ]
+            Daisy.cardBody [
+              prop.style [ style.alignItems.center ]
+              prop.children [
+                Daisy.cardTitle state.Pup.Name
+              ]
+            ]
+          ]
+        ]
+      ]
+    ]
     Daisy.button.button [
       prop.onClick (fun _ -> dispatch Increment)
       prop.text "Increment"
--- src/Main.fs
+++ src/Main.fs
@@ -5,7 +5,7 @@ open Elmish.React
 
 Fable.Core.JsInterop.importSideEffects "./index.css"
 
-Program.mkSimple App.init App.update App.render
+Program.mkProgram App.init App.update App.render
 |> Program.withConsoleTrace
 |> Program.withReactSynchronous "elmish-app"
 |> Program.run
